0323f1f023480a7eb003f1c68389e1a122c80d1b
hayasehiise <kuronegahiise@gmail.com>
2024-12-09 16:17:26 +0800

    navbar change

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,27 +1,107 @@
 "use client";
 import Image from "next/image";
 import BannerLogo from "@publicAsset/logo.webp";
+import React from "react";
+import Footer from "./footer";
 
-export default function Navbar() {
+export default function Navbar({children}: {children: React.ReactNode}) {
   return (
     <>
-      {/* <div className="navbar bg-base-100 text-white">
-        <div className="flex-1">
-          <a href="/">
-            <Image src={BannerLogo} alt="Banner" width={200} />
-          </a>
+      {/* <Navbar /> */}
+      <div className="drawer">
+          <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+          <div className="drawer-content flex flex-col">
+            {/* Navbar */}
+            <div className="navbar bg-base-300 w-full">
+              <div className="flex-none lg:hidden">
+                <label
+                  htmlFor="my-drawer-3"
+                  aria-label="open sidebar"
+                  className="btn btn-square btn-ghost"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    className="inline-block h-6 w-6 stroke-current"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    ></path>
+                  </svg>
+                </label>
+              </div>
+              <div className="mx-2 flex-1 px-2">
+                <a href="/">
+                  <Image src={BannerLogo} alt="Banner" width={200} />
+                </a>
+              </div>
+              <div className="hidden flex-none lg:block">
+                <ul className="menu menu-horizontal">
+                  {/* Navbar menu content here */}
+                  <li>
+                    <a href="/galery">
+                    <Image src={'/event-galery.webp'} alt="icon" width={20} height={20} />
+                    Gallery
+                    </a>
+                  </li>
+                  <li>
+                    <a href="/about">
+                    <Image src={'/about-us.webp'} alt="icon" width={20} height={20} />
+                    About Us
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            {/* Page content here */}
+            <PageContent>
+              {children}
+            </PageContent>
+            <Footer/>
+          </div>
+          <div className="drawer-side z-50">
+            <label
+              htmlFor="my-drawer-3"
+              aria-label="close sidebar"
+              className="drawer-overlay"
+            ></label>
+            <ul className="menu bg-base-200 min-h-dvh w-80 p-4">
+              {/* Sidebar content here */}
+              <li>
+                <a href="/">
+                <Image src={'/home.webp'} alt="icon" width={20} height={20} />
+                Home
+                </a>
+              </li>
+              <li>
+                <a href="/galery">
+                <Image src={'/event-galery.webp'} alt="icon" width={20} height={20} />
+                Gallery
+                </a>
+              </li>
+              <li>
+                <a href="/about">
+                <Image src={'/about-us.webp'} alt="icon" width={20} height={20} />
+                About Us
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div className="flex-none">
-          <ul className="menu menu-horizontal px-1">
-          <li>
-              <a href="/galery">Galery</a>
-            </li>
-            <li>
-              <a href="/about">About Us</a>
-            </li>
-          </ul>
-        </div>
-      </div> */}
     </>
   );
+}
+
+export function PageContent({children}: {children: React.ReactNode}){
+  return(
+    <>
+    <main>
+      {children}
+    </main>
+    </>
+  )
 }
